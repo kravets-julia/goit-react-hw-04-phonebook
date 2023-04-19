257327e3cb5453a85c183b7efd2b5ced84bdb28c
kravets-julia <kravets_julia@i.ua>
2023-04-19 20:25:22 +0300

    corected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,17 @@ import { FilterContacts } from './FilterContacts/FilterContacts';
 
 import css from '../components/App.module.css';
 
-const useLocalStorage = (key, defaultValue) => {
-  const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-  });
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-  return [state, setState];
-};
-
 export function App() {
+  const useLocalStorage = (key, defaultValue) => {
+    const [state, setState] = useState(() => {
+      return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+    });
+    useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+    return [state, setState];
+  };
+
   const [contacts, setContacts] = useLocalStorage('contacts', '');
   const [filter, setFilter] = useState('');
 
